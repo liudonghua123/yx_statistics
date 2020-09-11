76d68a89a92eaa141185b5ab19e6802ff8b3e8bb
liudonghua123 <liudonghua123@gmail.com>
2020-09-11 15:20:13 +0800

    lazy load font

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:artemis/artemis.dart';
+import 'package:flutter/services.dart';
 import 'package:yx_statistics_app/pages/home_page.dart';
+import 'package:yx_statistics_app/service.dart';
 import 'graphql/graphql_schema.dart';
 import 'flutter_configuration.dart';
 
@@ -13,14 +15,38 @@ void main() async {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  MyApp({Key key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String fontFamily;
+
+  @override
+  void initState() {
+    super.initState();
+    loadFont();
+  }
+
+  void loadFont() async {
+    var customFontFamily = 'romantic_garden';
+    var fontLoader = FontLoader(customFontFamily);
+    fontLoader.addFont(rootBundle.load('assets/fonts/romantic_garden.ttf'));
+    await fontLoader.load();
+    setState(() {
+      fontFamily = customFontFamily;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '迎新数据统计',
       theme: ThemeData(
-        fontFamily: "romantic_garden",
+        fontFamily: fontFamily,
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
